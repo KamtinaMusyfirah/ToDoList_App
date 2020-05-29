8586c3ddaf0d0be3d270badc1dee4d44865b9b4f
KamtinaMusyfirah <kamtinamusyfirah12@gmail.com>
2020-05-29 19:25:48 +0800

    CRUD ToDo

---
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.todolistapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.SparseBooleanArray
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var itemlist = arrayListOf<String>()
+        var adapter = ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_multiple_choice, itemlist)
+
+        add.setOnClickListener {
+            itemlist.add(editText.text.toString())
+            listView.adapter = adapter
+            adapter.notifyDataSetChanged()
+            //This is because every time when you add the item the input space
+            //or the edit text space will be cleared
+            editText.text.clear()
+        }
+        update.setOnClickListener {
+        }
+
+        delete.setOnClickListener {
+            val position: SparseBooleanArray = listView.checkedItemPositions
+            val count = listView.count
+            var item = count - 1
+            while (item >= 0){
+                if (position.get(item)){
+                    adapter.remove(itemlist.get(item))
+                }
+                item--
+            }
+            position.clear()
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
